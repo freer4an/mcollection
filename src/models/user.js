98433c2eb1fd2620e3123b8b38cf41d18d36a637
freer4an <freer4an@gmail.com>
2024-03-17 15:28:40 +0500

    sequelize user model define

---
+++ src/models/user.js
@@ -0,0 +1,62 @@
+import {DataTypes, ValidationError} from 'sequelize'
+import {hashSync} from 'bcrypt'
+import db from '../config/db.js'
+
+export const User = db.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING(16),
+            allowNull: false,
+            unique: true,
+            validate: {
+                is: {
+                    args: /^[A-Za-z][A-Za-z0-9]{2,15}$/g,
+                    msg: 'Username must start with a letter, be 3-15 characters long, and contain only letters and numbers.'
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING(64),
+            validate: {
+                len: {
+                    args: [6, 30],
+                    msg: 'Password must be between 6 and 30 characters'
+                }
+            }
+        },
+        role: {
+            type: DataTypes.ENUM('admin', 'user'),
+            defaultValue: 'user'
+        },
+        status: {
+            type: DataTypes.ENUM('active', 'blocked'),
+            defaultValue: 'active'
+        }
+    },
+    {
+        freezeTableName: true
+    })
+
+User.beforeCreate((user, options) => {
+    user.password = hashSync(user.password, 5)
+})
+
+User.afterFind((user, options) => {
+    delete user.password
+})
+
+User.sync({force: false}).then(() => {
+    console.log("User table created successfully")
+})
