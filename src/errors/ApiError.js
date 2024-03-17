86af2d06b6bb9d93c1bedd4209f2a243e9b36b8d
freer4an <freer4an@gmail.com>
2024-03-17 15:29:14 +0500

    api error class

--- src/errors/ApiError.js
+++ src/errors/ApiError.js
@@ -1,7 +1,18 @@
-class ApiError extends Error {
-    constructor(message, errors = []) {
+export class ApiError extends Error {
+    constructor(code, message) {
         super(message);
-        this.errors = errors;
+        this.code = code;
     }
-    
+
+    // static badRequest(message) {
+    //     return new ApiError(400, message);
+    // }
+    //
+    // static notFound(message) {
+    //     return new ApiError(404, message);
+    // }
+    //
+    // static alreadyExists(message) {
+    //     return new ApiError(409, message);
+    // }
 }
